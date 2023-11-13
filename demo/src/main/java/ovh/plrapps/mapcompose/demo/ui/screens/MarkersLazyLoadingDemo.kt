088f2d9c4cb692286865138af4d88e88163ab047
Christoph Wiesner <christoph.wiesner@siteplan.at>
2023-11-13 10:33:34 +0100

    Demo for marker update

--- demo/src/main/java/ovh/plrapps/mapcompose/demo/ui/screens/MarkersLazyLoadingDemo.kt
+++ demo/src/main/java/ovh/plrapps/mapcompose/demo/ui/screens/MarkersLazyLoadingDemo.kt
@@ -1,12 +1,44 @@
 package ovh.plrapps.mapcompose.demo.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Slider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import ovh.plrapps.mapcompose.demo.viewmodels.MarkersLazyLoadingVM
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import ovh.plrapps.mapcompose.demo.viewmodels.MarkersLazyLoadingVM
 import ovh.plrapps.mapcompose.ui.MapUI
 
 @Composable
-fun MarkersLazyLoadingDemo(modifier: Modifier = Modifier, viewModel: MarkersLazyLoadingVM = viewModel()) {
-    MapUI(modifier, state = viewModel.state)
+fun MarkersLazyLoadingDemo(
+    modifier: Modifier = Modifier,
+    viewModel: MarkersLazyLoadingVM = viewModel()
+) {
+    var rotation by remember {
+        mutableStateOf(0f)
+    }
+
+    LaunchedEffect(rotation) {
+        viewModel.onRotationUpdate(rotation)
+    }
+
+    Box {
+        MapUI(modifier.padding(bottom = 80.dp), state = viewModel.state)
+        Slider(
+            value = rotation, onValueChange = {
+                rotation = it
+            }, valueRange = 0f..360f, modifier = Modifier.padding(16.dp)
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+        )
+    }
+
 }
--- demo/src/main/java/ovh/plrapps/mapcompose/demo/viewmodels/MarkersLazyLoadingVM.kt
+++ demo/src/main/java/ovh/plrapps/mapcompose/demo/viewmodels/MarkersLazyLoadingVM.kt
@@ -1,20 +1,31 @@
 package ovh.plrapps.mapcompose.demo.viewmodels
 
 import android.app.Application
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.AndroidViewModel
-import ovh.plrapps.mapcompose.api.*
-import ovh.plrapps.mapcompose.demo.R
+import ovh.plrapps.mapcompose.api.ExperimentalClusteringApi
+import ovh.plrapps.mapcompose.api.addLayer
+import ovh.plrapps.mapcompose.api.addLazyLoader
+import ovh.plrapps.mapcompose.api.addMarker
+import ovh.plrapps.mapcompose.api.onMarkerClick
+import ovh.plrapps.mapcompose.api.removeAllMarkers
+import ovh.plrapps.mapcompose.api.shouldLoopScale
 import ovh.plrapps.mapcompose.ui.layout.Forced
 import ovh.plrapps.mapcompose.ui.state.MapState
 import ovh.plrapps.mapcompose.ui.state.markers.model.RenderingStrategy
+import ovh.plrapps.mapcompose.utils.Point
 import kotlin.random.Random
 
 /**
@@ -22,6 +33,17 @@ import kotlin.random.Random
  */
 @OptIn(ExperimentalClusteringApi::class)
 class MarkersLazyLoadingVM(application: Application) : AndroidViewModel(application) {
+
+    var viewRotation = 0f
+
+    val points = mutableListOf<Point>()
+
+    fun onRotationUpdate(rotation: Float) {
+        viewRotation = rotation
+        state.removeAllMarkers()
+        addMarkers()
+    }
+
     private val tileStreamProvider =
         ovh.plrapps.mapcompose.demo.providers.makeTileStreamProvider(application.applicationContext)
 
@@ -41,38 +63,44 @@ class MarkersLazyLoadingVM(application: Application) : AndroidViewModel(applicat
         /* Add a marker lazy loader. In this example, we use "default" for the id */
         state.addLazyLoader("default")
 
-        repeat(200) { i ->
-            val x = Random.nextDouble()
-            val y = Random.nextDouble()
-
-            /* Notice how we set the rendering strategy to lazy loading with the same id */
-            state.addMarker(
-                "marker-$i", x, y,
-                renderingStrategy = RenderingStrategy.LazyLoading(lazyLoaderId = "default")
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.map_marker),
-                    contentDescription = null,
-                    modifier = Modifier.size(50.dp),
-                    tint = Color(0xEE2196F3)
-                )
-            }
-        }
-
-        /* We can still add regular markers */
-        state.addMarker(
-            "marker-regular", 0.5, 0.5,
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.map_marker),
-                contentDescription = null,
-                modifier = Modifier.size(50.dp),
-                tint = Color(0xEEF44336)
-            )
-        }
-
+        generateLocations()
+        addMarkers()
         state.onMarkerClick { id, x, y ->
             println("marker click $id $x $y")
         }
     }
+
+    private fun generateLocations() {
+        points.clear()
+        repeat(200) { i ->
+            points.add(Point(Random.nextDouble(), Random.nextDouble()))
+        }
+    }
+
+    private fun addMarkers() {
+        points.mapIndexed { index, point ->
+            /* Notice how we set the rendering strategy to lazy loading with the same id */
+            state.addMarker(
+                "marker-$index", point.x, point.y,
+                renderingStrategy = RenderingStrategy.LazyLoading(lazyLoaderId = "default")
+            ) {
+                Box(
+                    Modifier
+                        .size(40.dp)
+                        .background(Color.Blue)
+                ) {
+                    Icon(
+                        tint = Color.White,
+                        painter = rememberVectorPainter(image = Icons.Rounded.ArrowBack),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(50.dp)
+                            .rotate(viewRotation),
+                    )
+                }
+            }
+        }
+
+    }
 }
+
